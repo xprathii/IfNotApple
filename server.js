812098473cdd70058139945c3b1adeb6b7db47a6
Nikunj Gagrani <104256141+nikk-04@users.noreply.github.com>
2024-03-30 11:52:40 +0530

    final deploy changes

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.use("/api/admin", adminRoute);
 app.use("/api/doctor",doctorRoute);
 const port = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === 'production'){
+    app.use('/',express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client/build/index.html'));
+    });
+}
 
-
-app.listen(port, ()=> console.log('Listening on port ' + port))
+app.listen(port, ()=> console.log('Listening on port'+port))
 
